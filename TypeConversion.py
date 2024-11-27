9df35f541e7a02ca93c61a54a4ba4ad2254a4fcc
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2024-11-27 19:08:12 +0530

    Type Conversion

---
+++ TypeConversion.py
@@ -0,0 +1,16 @@
+#  There are two types of conversion 
+    # 1. Automatic conversion done
+    # 2. Manual conversion done using the  function
+    
+# 1.Automatic conversion
+a = 2
+b = 4.25
+
+print(a+b) # Authomatically convert the value of 'a' (integer) to the (float) hence we get 6.25
+
+# 2. Manual conversion
+
+a = int("1")
+b = 2
+print(type(a))
+print(a+b) # Manually convert the string "1" to an integer (1) and then add it to the integer (2) 
