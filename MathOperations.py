66701c34c1142f9e6fa222e8ced3374d19b32617
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2024-11-27 18:37:55 +0530

    Math Operations

---
+++ MathOperations.py
@@ -0,0 +1,29 @@
+#  Following are the basic Math Operation (+ , - , * , / )
+
+#  Addition Operators (+)
+a = 10
+b = 20
+result = a + b
+# print(result) Can also do like this 
+print(f"Addition of {a} and {b} is: {result}")
+
+
+#  Subtraction Operators (-)
+a = 10
+b = 20
+result = a - b
+print(f"Subtraction of {a} and {b} is: {result}")
+
+
+#  Multiplication Operators (*)
+a = 10
+b = 20
+result = a * b
+print(f"Multiplication of {a} and {b} is: {result}")
+
+
+#  Division Operators (/)
+a = 10
+b = 20
+result = a / b
+print(f"Division of {a} and {b} is: {result}")
