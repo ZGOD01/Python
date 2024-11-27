5ff474bce697e4afadb7789791e758a7b16ee592
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2024-11-27 18:58:39 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,56 @@
+# Operators
+
+# Arithmetic Operators: +, -, *, /, %, **
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a ** b)  #a^b
+
+
+# Relational operators : (==, !=, > , < , >= , <= )
+
+a = 50
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+# Assignment Operators (= , +=, -=, /=, %=, **=)
+a = 1
+
+a += 5
+print(a)
+
+b =5
+b -= 1
+print(b)
+
+c = 10 
+c /= 1
+print(c)
+
+d = 50
+d %= 100
+print(d) # remainder will calculate
+
+e = 10
+e **= 5
+print(e) # 10^5
+
+
+#  Logical Operators (not , and , or)
+
+print(not False) # True (Changes the Value )
+print(True and False) # False (Works on two operands)
+print(True or False) # True (Works on if at least one of the values is True)
+
