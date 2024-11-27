b97b5d4fceb4bf1b687c959484dab73bb7c45579
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2024-11-27 18:31:33 +0530

    Data Types

---
+++ DataTypes.py
@@ -0,0 +1,23 @@
+
+# -------------------Following are some of Data Types in Python------------------- #
+
+
+#----->  String 
+name="ZGOD"
+print(type(name))
+
+# ---> Number 
+age = 23
+print(type(age))
+
+# ---> Float
+marks = 12.1
+print(type(marks))
+
+# ---> Boolean
+old = False
+print(type(old))
+
+# ---> None
+value = None
+print(type(value))
